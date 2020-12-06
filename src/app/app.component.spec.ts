399365c9d83e840adbf0e82ff599d0467f2ace6a
Willardy Tyrone <willardy@gmail.com>
2020-12-06 19:00:17 -0300

    Configurando o Bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular11-gerenciador-de-tarefas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular11-gerenciador-de-tarefas');
+    expect(app.title).toEqual('Gerenciador de Tarefas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular11-gerenciador-de-tarefas app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Gerenciador de Tarefas');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular11-gerenciador-de-tarefas';
+  title = 'Gerenciador de Tarefas';
 }
