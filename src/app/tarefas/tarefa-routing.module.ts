332dfabb315dcb8a2ea1ff7e629f229313d0efc7
Willardy Tyrone <willardy@gmail.com>
2020-12-08 21:20:53 -0300

    Implementando Cadastro de Tarefas

--- src/app/tarefas/tarefa-routing.module.ts
+++ src/app/tarefas/tarefa-routing.module.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 
 import {ListarTarefaComponent} from './listar';
+import {CadastrarTarefaComponent} from './cadastrar';
 
 export const TarefaRoutes: Routes = [
   {
@@ -10,5 +11,9 @@ export const TarefaRoutes: Routes = [
   {
     path: 'tarefas/listar',
     component: ListarTarefaComponent
+  },
+  {
+    path: 'tarefas/cadastrar',
+    component: CadastrarTarefaComponent
   }
 ];
